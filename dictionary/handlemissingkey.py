9acb9295b3426042c46d42ab6e8ba6d5ff8252c8
mondeep31 <97219431+mondeep31@users.noreply.github.com>
2024-01-25 23:59:46 +0530

    Delete dictionary directory

--- dictionary/handlemissingkey.py
+++
@@ -1,6 +0,0 @@
-my_dict = {'a': 10, 'b': 20, 'c': 30}
-
-# Handling missing key using get()
-key_to_check = 'd'
-value = my_dict.get(key_to_check, "Key not found")
-print(value)
--- dictionary/sortlambda.py
+++
@@ -1,5 +0,0 @@
-list_of_dicts = [{'name': 'Alice', 'age': 25}, {'name': 'Bob', 'age': 20}, {'name': 'Charlie', 'age': 30}]
-
-# Sorting by 'age' key
-sorted_list = sorted(list_of_dicts, key=lambda x: x['age'])
-print(sorted_list)
